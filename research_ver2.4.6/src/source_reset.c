8a333144fdc921a762f35fb1ac1a6beb5c7628b5
kenty <tktkdx.62000@gmail.com>
2020-01-13 09:46:11 +0900

    station

---
+++ research_ver2.4.6/src/source_reset.c
@@ -0,0 +1,19 @@
+
+#include "function.h"
+
+/*TEMP_IMMEDIATELYをSTILLに戻すための関数*/
+
+void source_reset(struct Node nn[ONE_SIDE][ONE_SIDE]){
+	
+	int i;
+	
+	//情報共有者リストを初期化
+	allDeleteList(head);
+	head=NULL;
+	tail=head;
+	
+	for(i=0;i<ONE_SIDE*ONE_SIDE;i++){
+		if(nn[i/ONE_SIDE][i%ONE_SIDE].source_of_infection == TEMP_IMMEDIATELY)
+			nn[i/ONE_SIDE][i%ONE_SIDE].source_of_infection = STILL;
+	}
+}
